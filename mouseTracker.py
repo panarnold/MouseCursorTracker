e8da52d0f5e9927aed0bec5598ae910819690c2c
Arnold Cytrowski <cytrowskiarnold@gmail.com>
2020-11-11 20:13:13 +0100

    now its super fine and super extra with super extra update

--- mouseTracker.py
+++ mouseTracker.py
@@ -1,5 +1,6 @@
 #! python
 # mouseTrackerpy - prints current position of mouse cursor to the screen
+# update: it also get info about color of current pixel in RGB
 # XI 2020 Arnold Cytrowski
 
 import pyautogui
@@ -8,7 +9,8 @@ print('Press Ctrl+C to finish the program')
 try:
     while True:
         x, y = pyautogui.position()
-        position_str = f'X: {str(x).rjust(4)}, Y: {str(y).rjust(4)}'
+        pixel_color = pyautogui.screenshot().getpixel((x,y))
+        position_str = f'X: {str(x).rjust(4)}, Y: {str(y).rjust(4)} RGB: ({str(pixel_color[0]).rjust(3)}, {str(pixel_color[1]).rjust(3)}, {str(pixel_color[2]).rjust(3)})'
         print(position_str, end='')
         print('\b' * len(position_str), end='', flush=True)
 except KeyboardInterrupt:
